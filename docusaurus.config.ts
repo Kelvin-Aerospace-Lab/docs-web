b5627ac333b721e1a23f9d00d2b2234ae8417c2c
enthalpiex <kelvinaero@proton.me>
2025-10-08 22:20:42 -0500

    1

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -26,10 +26,10 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'Kelvin-Aerospace-Lab', // Usually your GitHub org/user name.
+  projectName: 'KALDOC', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
